9395c8dc25644ffe227f370d3d508c5166935add
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-01-31 14:22:25 +0100

    Act 24

--- makeWin.ps1
+++ makeWin.ps1
@@ -1,7 +1,26 @@
+# Eliminar carpetas y archivos previos antes de construir
+if (Test-Path -Path "node_modules") {
+    Write-Host "Eliminando node_modules..."
+    Remove-Item -Path "node_modules" -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+if (Test-Path -Path "dist") {
+    Write-Host "Eliminando dist..."
+    Remove-Item -Path "dist" -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+if (Test-Path -Path "package-lock.json") {
+    Write-Host "Eliminando package-lock.json..."
+    Remove-Item -Path "package-lock.json" -Force -ErrorAction SilentlyContinue
+}
+
+npm i
+
+# Continuar con el resto del script
 npm run build:win
 
 # Reemplazar espacios por guiones en los nombres de archivo .exe y .blockmap generados
-Get-ChildItem -Path . -Recurse -Include '*.exe', '*.blockmap' | ForEach-Object { 
+Get-ChildItem -Path . -Recurse -Include '*.exe', '*.blockmap' | ForEach-Object {
     $newName = $_.Name -replace ' ', '-'
     Rename-Item -Path $_.FullName -NewName $newName
 }
