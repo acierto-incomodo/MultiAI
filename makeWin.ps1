ec35657f9e4afc6085af1c814708195e5d1b6a9e
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-01-31 13:04:23 +0100

    Act 21

--- makeWin.ps1
+++ makeWin.ps1
@@ -1,1 +1,7 @@
 npm run build:win
+
+# Reemplazar espacios por guiones en los nombres de archivo .exe y .blockmap generados
+Get-ChildItem -Path . -Recurse -Include '*.exe', '*.blockmap' | ForEach-Object { 
+    $newName = $_.Name -replace ' ', '-'
+    Rename-Item -Path $_.FullName -NewName $newName
+}
