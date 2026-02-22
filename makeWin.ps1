b2e2538953dc10eb0b6ad4cf5a0705ec33370501
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-02-22 17:00:56 +0100

    Act 49

--- makeWin.ps1
+++ makeWin.ps1
@@ -1,20 +1,3 @@
-# Eliminar carpetas y archivos previos antes de construir
-if (Test-Path -Path "node_modules") {
-    Write-Host "Eliminando node_modules..."
-    Remove-Item -Path "node_modules" -Recurse -Force -ErrorAction SilentlyContinue
-}
-
-if (Test-Path -Path "dist") {
-    Write-Host "Limpiando directorio dist, pero conservando latest-linux.yml..."
-    # Elimina todo dentro de 'dist' excepto 'latest-linux.yml'
-    Get-ChildItem -Path "dist" -Exclude "latest-linux.yml" | Remove-Item -Recurse -Force
-}
-
-if (Test-Path -Path "package-lock.json") {
-    Write-Host "Eliminando package-lock.json..."
-    Remove-Item -Path "package-lock.json" -Force -ErrorAction SilentlyContinue
-}
-
 npm i
 
 # Continuar con el resto del script
