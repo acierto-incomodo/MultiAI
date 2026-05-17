1f7ab8ded8fbf2d213b5ee935de0aff2b07f25f4
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-17 15:51:02 +0200

    Act 66

---
+++ make-v1.sh
@@ -0,0 +1,33 @@
+npm install
+
+sleep 1
+
+echo "Iniciando build AppImage..."
+./makeAppImage.sh
+echo "¡Proceso de build completado con éxito!"
+echo "AppImage Finalizado"
+
+sleep 1
+
+echo "Iniciando build DEB..."
+./makeDeb.sh
+echo "¡Proceso de build completado con éxito!"
+echo "DEB Finalizado"
+
+sleep 1
+
+echo "Iniciando build SNAP..."
+./makeSnap.sh
+echo "¡Proceso de build y subida completado con éxito!"
+echo "SNAP Finalizado"
+
+sleep 1
+
+echo "Iniciando build Arch (Pacman)..."
+./makeArch.sh
+echo "¡Proceso de build completado con éxito!"
+echo "Arch Finalizado"
+
+sleep 1
+
+echo "Build general completado con éxito."
--- make.sh
+++ make.sh
@@ -1,33 +1,21 @@
-npm install
+#!/bin/bash
 
-sleep 1
+# Script para construir paquete AppImage.
 
-echo "Iniciando build AppImage..."
-./makeAppImage.sh
+# Termina el script inmediatamente si algún comando falla.
+set -e
+
+echo "Limpiando archivos Linux anteriores de la carpeta dist/..."
+rm -f dist/*.AppImage
+rm -f dist/*.deb
+rm -f dist/*.pkg.tar.zst
+rm -f dist/*.pkg.tar.xz
+rm -f dist/*.flatpak
+rm -f dist/*.snap
+rm -rf dist/linux-unpacked
+rm -f dist/latest-linux.yml
+
+echo "Ejecutando la build..."
+npm run build:linux:all
+
 echo "¡Proceso de build completado con éxito!"
-echo "AppImage Finalizado"
-
-sleep 1
-
-echo "Iniciando build DEB..."
-./makeDeb.sh
-echo "¡Proceso de build completado con éxito!"
-echo "DEB Finalizado"
-
-sleep 1
-
-echo "Iniciando build SNAP..."
-./makeSnap.sh
-echo "¡Proceso de build y subida completado con éxito!"
-echo "SNAP Finalizado"
-
-sleep 1
-
-echo "Iniciando build Arch (Pacman)..."
-./makeArch.sh
-echo "¡Proceso de build completado con éxito!"
-echo "Arch Finalizado"
-
-sleep 1
-
-echo "Build general completado con éxito."
